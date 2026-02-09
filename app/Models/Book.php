6313bf15af167d695a5b03c90ddb04b8a17ffd8a
phonemyattayzarkyaw <phonemyat.t.z.kyaw@frontiir.net>
2026-02-09 10:22:10 +0630

    add all data for models

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -9,4 +9,43 @@ class Book extends Model
 {
     /** @use HasFactory<\Database\Factories\BookFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'isbn',
+        'description',
+        'author_id',
+        'genre',
+        'published_at',
+        'total_copies',
+        'available_copies',
+        'cover_image',
+        'price',
+        'status',
+    ];
+
+
+    public function author()
+    {
+        return $this->belongsTo(Author::class);
+    }
+
+    public function isAvailable()
+    {
+        return $this->available_copies > 0;
+    }
+
+    public function borrowings()
+    {
+        return $this->hasMany(Borrowing::class);
+    }
+
+    public function returnBook():void
+    {
+        if($this->available_copies < $this->total_copies) {
+            $this->increment('available_copies');
+        }
+    }
 }
+
+
--- app/Models/Borrowing.php
+++ app/Models/Borrowing.php
@@ -4,9 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Carbon\Carbon;
 
 class Borrowing extends Model
 {
     /** @use HasFactory<\Database\Factories\BorrowingFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'member_id',
+        'borrowed_date',
+        'due_date',
+        'returned_date',
+        'status'
+    ];
+
+    protected $casts = [
+        'borrowed_date' => 'date',
+        'due_date' => 'date',
+        'returned_date' => 'date',
+    ];
+    
+    public function book(): BelongsTo
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function member(): BelongsTo
+    {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function isOverdue(): bool
+    {
+        return $this->due_date < Carbon::today() && $this->status === 'borrowed';
+    }
 }
